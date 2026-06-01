1660ccbe0cb81dce7e378304dc7d49d4289af398
faizantanwar16 <faizantanwar1611@gmail.com>
2026-06-01 10:57:13 +0530

    feat(auth): add JWT authentication middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
 const authRoutes = require("./routes/authRoutes");
 const connectDB = require("./config/db");
 
@@ -8,15 +9,20 @@ dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("Smart Habit Tracker API Running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
+// Start Server
 const startServer = async () => {
   try {
     await connectDB();
